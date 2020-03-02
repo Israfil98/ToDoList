eac16259f06293895f1d2a833ebd73ad9cf02947
Israfil <zorro01@inbox.ru>
2020-03-02 20:40:23 +0300

    ToDo List app done

--- ToDoList/TableViewController.swift
+++ ToDoList/TableViewController.swift
@@ -100,7 +100,7 @@ class TableViewController: UITableViewController {
             tasks.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
     }
 }
